40e4bd7599a241994f95092b48e2da55d0afd63b
Luke Belinc <belinc@tuta.io>
2021-11-15 13:43:28 -0500

    added variable examples

--- VarDemo/Program.cs
+++ VarDemo/Program.cs
@@ -6,7 +6,16 @@ namespace VarDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello \"World!");
+            Console.WriteLine("Hello world line 2");
+
+            string message = "hello world";
+            Console.WriteLine(message);
+
+            int AppleCount = 25;
+            Console.WriteLine(AppleCount);
+            Console.WriteLine(AppleCount + 1);
+            Console.WriteLine(AppleCount);
         }
     }
 }
