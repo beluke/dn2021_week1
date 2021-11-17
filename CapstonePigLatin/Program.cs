718280d71d045b54d4578a953a7799bdc20b27fc
Luke Belinc <belinc@tuta.io>
2021-11-17 14:17:13 -0500

    CapstonePigLatin project intial commit

---
+++ CapstonePigLatin/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CapstonePigLatin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
