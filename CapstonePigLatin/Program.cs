3f3bdab25ef7926295fcb3dd820e535c6cb88a94
Luke Belinc <belinc@tuta.io>
2021-11-17 15:23:12 -0500

    program.cs prints pig latin translation
    - added linq library
    - split input string into array using space as delimiter
    - using select on each word in input.Split() array
    - updated array is joined by spaces
    - added "continue translating" prompt

--- CapstonePigLatin/Program.cs
+++ CapstonePigLatin/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace CapstonePigLatin
 {
@@ -6,7 +8,21 @@ namespace CapstonePigLatin
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("\nWelcome to the Pig Latin Translator!");
+
+            do
+            {
+                Console.Write("\nEnter a line to be translated: ");
+                string input = Console.ReadLine().ToLower();
+
+                Console.WriteLine("\nTranslation: " + string.Join(" ", input.Split(' ').Select(x => !"aeiou".Contains(x[0]) ? x[1..] + x[0] + "ay" : x + "way")));
+
+                Console.Write("\nTranslate another line? ([y]es/[n]o) (default = no): ");
+                input = Console.ReadLine().ToLower();
+
+                if (input != "y" && input != "yes") break;
+
+            } while (true);
         }
     }
 }
