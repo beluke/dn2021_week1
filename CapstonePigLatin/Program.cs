33fb3c230b47f4477e664ec27c024a87c2366e29
Luke Belinc <belinc@tuta.io>
2021-11-17 17:08:44 -0500

    added extra specifications to translation

--- CapstonePigLatin/Program.cs
+++ CapstonePigLatin/Program.cs
@@ -16,7 +16,13 @@ namespace CapstonePigLatin
             {
                 // prompt user for input
                 Console.Write("\nEnter a line to be translated: ");
-                string input = Console.ReadLine().ToLower();
+                string input = Console.ReadLine();
+
+                if (input.Length <= 0)
+                {
+                    Console.WriteLine("\nPlease dont input empty strings...");
+                    continue;
+                }
 
                 // convert string into pig latin
                 // * separate string by spaces into array
@@ -25,9 +31,37 @@ namespace CapstonePigLatin
                 // * generate new strings accordingly
                 // * join string array returned from select using spaces
                 // * finally, print output
-                Console.WriteLine("\nTranslation: " + string.Join(" ", input.Split(' ').Select(word => { 
+                Console.WriteLine("\nTranslation: " + string.Join(" ", input.Split(' ').Select(word => {
+                    // if word matches special character or has numbers, just return it
+                    if (Regex.IsMatch(word, "([0-9@])")) return word;
+
+                    // case flags
+                    bool titleCase = false;
+                    bool allUpper = false;
+
+                    // update case flags
+                    if (word.Equals(word.ToUpper())) allUpper = true;
+                    else if (word[0].Equals(char.ToUpper(word[0]))) titleCase = true;
+
+                    // get length of beginning consonant cluster
                     int idx = Regex.Match(word, "^([^aeiou]*)").Length;
-                    return idx > 0 ? word[idx..] + word[0..idx] + "ay" : word + "way";
+
+                    // generate new string while minding location of period
+                    // if period is in middle, ignore it, otherwise add it to end
+                    if (word.IndexOf(".") == word.Length - 1)
+                        word = idx > 0 ? word[idx..(word.Length - 1)] + word[0..idx] + "ay." : word[0..(word.Length - 1)] + "way.";
+                    else
+                        word = idx > 0 ? word[idx..] + word[0..idx] + "ay" : word[0..] + "way";
+
+                    // conver string to lower
+                    word = word.ToLower();
+                    
+                    // fix case
+                    if (titleCase) word = char.ToUpper(word[0]) + word[1..];
+                    else if (allUpper) word = word.ToUpper();
+
+                    // return word
+                    return word;
                 })));
 
                 // prompt to continue
